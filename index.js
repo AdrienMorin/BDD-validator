13007c40c5495b61327d0ba269f0df3b9b41d916
adrienmorin1 <adridu71170@gmail.com>
2023-10-20 22:21:03 +0200

    adding question 1 and 2

--- index.js
+++ index.js
@@ -15,10 +15,68 @@ let dataFromMail = "";
 
 app.get('/', async (req, res) => {
     try {
-        res.send(```Bienvenue dans l'API de validation du module BDD de la semaine de formation à l'informatique !
-        Pour envoyer une réponse, il suffit d'appeler l'API à l'URL actuelle suivie de :
-        /{numéro de question}/{réponse}
-        ```);
+        res.send(
+            "Bienvenue dans l'API de validation du module BDD de la semaine de formation à l'informatique !"
+        +"\nPour envoyer une réponse, il suffit d'appeler l'API à l'URL actuelle suivie de :"
+        +"\n/{numéro de question}/{réponse}"
+        );
+    } catch (error) {
+        console.error('Une erreur s\'est produite :', error);
+    }
+});
+
+app.get('/1/:reponse', async (req, res) => {
+    try {
+        if (req.params.reponse === "5"){
+            res.send(
+                "Bien joué !" +
+                "“ Bravo pour l’étape précédente. Maintenant, on va s'intéresser à la manière dont le coupable a ouvert ce coffre fort. Le fabricant du coffre pourra vous fournir des informations nécessaires. Contactez le fabricant du coffre.” - ADAPolice\n" +
+                "\n" +
+                "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n" +
+                "[Outil utilisé : SQL]\n" +
+                "[Table concernée : “coffrecreateurs”]\n" +
+                "\n" +
+                "Tips : Utiliser SELECT * pour cette étape car c’est une petite table.\n" +
+                "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n" +
+                "\n" +
+                "Cherchez le numéro de portable du fabricant du coffre et envoyez ça en réponse du mail ! \n"
+            );
+        } else {
+            res.send(
+                "Mauvaise réponse…"
+            );
+        }
+
+    } catch (error) {
+        console.error('Une erreur s\'est produite :', error);
+    }
+});
+
+app.get('/2/:reponse', async (req, res) => {
+    try {
+        if (req.params.reponse === "0645555555" || req.params.reponse === "645555555"){
+            res.send(
+                "Bien joué !" +
+                "“ Le coffre n’a pas de trace d’effraction. Comment le voleur a-t-il ouvert le coffre?” - ADAPolice\n" +
+                "“ Jetez un coup d'œil sur la table “coffreOuverture”. J’ai indiqué tous les détails expliquant comment le coffre peut être ouvert.” - Fabricant du coffre\n" +
+                "\n" +
+                "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n" +
+                "[Outil utilisé : SQL]\n" +
+                "[Table concernée : “coffreOuverture”]\n" +
+                "\n" +
+                "Tips : Signe d’effraction = “rien”\n" +
+                "Tips2. Vous allez finalement utiliser le “WHERE”\n" +
+                "\n" +
+                "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n" +
+                "\n" +
+                "Quel outil le coupable a donc utilisé pour ouvrir le coffre?\n"
+            );
+        } else {
+            res.send(
+                "Mauvaise réponse…"
+            );
+        }
+
     } catch (error) {
         console.error('Une erreur s\'est produite :', error);
     }
