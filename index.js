1f99e9c7014867aeca10a75408cdcd1cfa175dfe
rafael folgoas <rafael.folgoas@insa-lyon.fr>
2023-10-21 12:37:23 +0200

    chore: API reponses RAF

--- index.js
+++ index.js
@@ -17,8 +17,8 @@ app.get('/', async (req, res) => {
     try {
         res.send(
             "Bienvenue dans l'API de validation du module BDD de la semaine de formation à l'informatique !"
-        +"\nPour envoyer une réponse, il suffit d'appeler l'API à l'URL actuelle suivie de :"
-        +"\n/{numéro de question}/{réponse}" +
+            + "\nPour envoyer une réponse, il suffit d'appeler l'API à l'URL actuelle suivie de :"
+            + "\n/{numéro de question}/{réponse}" +
             "\nBonne chance !"
         );
     } catch (error) {
@@ -28,7 +28,7 @@ app.get('/', async (req, res) => {
 
 app.get('/1/:reponse', async (req, res) => {
     try {
-        if (req.params.reponse === "5"){
+        if (req.params.reponse === "5") {
             res.send(
                 "Bien joué !" +
                 "“ Bravo pour l’étape précédente. Maintenant, on va s'intéresser à la manière dont le coupable a ouvert ce coffre fort. Le fabricant du coffre pourra vous fournir des informations nécessaires. Contactez le fabricant du coffre.” - ADAPolice\n" +
@@ -55,7 +55,7 @@ app.get('/1/:reponse', async (req, res) => {
 
 app.get('/2/:reponse', async (req, res) => {
     try {
-        if (req.params.reponse === "0645555555" || req.params.reponse === "645555555"){
+        if (req.params.reponse === "0645555555" || req.params.reponse === "645555555") {
             res.send(
                 "Bien joué !" +
                 "“ Le coffre n’a pas de trace d’effraction. Comment le voleur a-t-il ouvert le coffre?” - ADAPolice\n" +
@@ -71,6 +71,440 @@ app.get('/2/:reponse', async (req, res) => {
                 "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n" +
                 "\n" +
                 "Quel outil le coupable a donc utilisé pour ouvrir le coffre?\n"
+            );
+        } else {
+            res.send(
+                "Mauvaise réponse…"
+            );
+        }
+
+    } catch (error) {
+        console.error('Une erreur s\'est produite :', error);
+    }
+});
+
+app.get('/3/:reponse', async (req, res) => {
+    try {
+        if (req.params.reponse === "Clé" || req.params.reponse === "Cle") {
+            res.send(
+                ""
+            );
+        } else {
+            res.send(
+                "Mauvaise réponse…"
+            );
+        }
+
+    } catch (error) {
+        console.error('Une erreur s\'est produite :', error);
+    }
+});
+
+app.get('/4/:reponse', async (req, res) => {
+    try {
+        if (req.params.reponse ==="11") {
+            res.send(
+                ""
+            );
+        } else {
+            res.send(
+                "Mauvaise réponse…"
+            );
+        }
+
+    } catch (error) {
+        console.error('Une erreur s\'est produite :', error);
+    }
+});
+app.get('/5/:reponse', async (req, res) => {
+    try {
+        if (req.params.reponse==="SELECT * FROM employes WHERE etage=11") {
+            res.send(
+                ""
+            );
+        } else {
+            res.send(
+                "Mauvaise réponse…"
+            );
+        }
+
+    } catch (error) {
+        console.error('Une erreur s\'est produite :', error);
+    }
+});
+app.get('/6/:reponse', async (req, res) => {
+    try {
+        if (req.params.reponse ==="5") {
+            res.send(
+                ""
+            );
+        } else {
+            res.send(
+                "Mauvaise réponse…"
+            );
+        }
+
+    } catch (error) {
+        console.error('Une erreur s\'est produite :', error);
+    }
+});
+app.get('/7/:reponse', async (req, res) => {
+    try {
+        if (req.params.reponse ==="0574538452"||req.params.reponse==="574538452") {
+            res.send(
+                ""
+            );
+        } else {
+            res.send(
+                "Mauvaise réponse…"
+            );
+        }
+
+    } catch (error) {
+        console.error('Une erreur s\'est produite :', error);
+    }
+});
+app.get('/8/:reponse', async (req, res) => {
+    try {
+        if (req.params.reponse ==="erlen.meyer@sbank.fr") {
+            res.send(
+                ""
+            );
+        } else {
+            res.send(
+                "Mauvaise réponse…"
+            );
+        }
+
+    } catch (error) {
+        console.error('Une erreur s\'est produite :', error);
+    }
+});
+app.get('/9/:reponse', async (req, res) => {
+    try {
+        if (req.params.reponse === "Oui" || req.params.reponse === "oui") {
+            res.send(
+                ""
+            );
+        } else {
+            res.send(
+                "Mauvaise réponse…"
+            );
+        }
+
+    } catch (error) {
+        console.error('Une erreur s\'est produite :', error);
+    }
+});
+app.get('/10/:reponse', async (req, res) => {
+    try {
+        if (req.params.reponse === "Bague en diamant" || req.params.reponse === "bague en diamant") {
+            res.send(
+                ""
+            );
+        } else {
+            res.send(
+                "Mauvaise réponse…"
+            );
+        }
+
+    } catch (error) {
+        console.error('Une erreur s\'est produite :', error);
+    }
+});
+app.get('/11/:reponse', async (req, res) => {
+    try {
+        if (req.params.reponse === "ABAMA Baraque" ) {
+            res.send(
+                ""
+            );
+        } else {
+            res.send(
+                "Mauvaise réponse…"
+            );
+        }
+
+    } catch (error) {
+        console.error('Une erreur s\'est produite :', error);
+    }
+});
+app.get('/12/:reponse', async (req, res) => {
+    try {
+        if (req.params.reponse === "0875468545" || req.params.reponse === "875468545") {
+            res.send(
+                ""
+            );
+        } else {
+            res.send(
+                "Mauvaise réponse…"
+            );
+        }
+
+    } catch (error) {
+        console.error('Une erreur s\'est produite :', error);
+    }
+});
+app.get('/13/:reponse', async (req, res) => {
+    try {
+        if (req.params.reponse === "ayayabama@sbank.fr" ) {
+            res.send(
+                ""
+            );
+        } else {
+            res.send(
+                "Mauvaise réponse…"
+            );
+        }
+
+    } catch (error) {
+        console.error('Une erreur s\'est produite :', error);
+    }
+});
+app.get('/14/:reponse', async (req, res) => {
+    try {
+        if (req.params.reponse === "bob725@gmail.com" ) {
+            res.send(
+                ""
+            );
+        } else {
+            res.send(
+                "Mauvaise réponse…"
+            );
+        }
+
+    } catch (error) {
+        console.error('Une erreur s\'est produite :', error);
+    }
+});
+app.get('/15/:reponse', async (req, res) => {
+    try {
+        if (req.params.reponse === "ABABA95" ) {
+            res.send(
+                ""
+            );
+        } else {
+            res.send(
+                "Mauvaise réponse…"
+            );
+        }
+
+    } catch (error) {
+        console.error('Une erreur s\'est produite :', error);
+    }
+});
+app.get('/16/:reponse', async (req, res) => {
+    try {
+        if (req.params.reponse === "10/04/2022") {
+            res.send(
+                ""
+            );
+        } else {
+            res.send(
+                "Mauvaise réponse…"
+            );
+        }
+
+    } catch (error) {
+        console.error('Une erreur s\'est produite :', error);
+    }
+});
+app.get('/17/:reponse', async (req, res) => {
+    try {
+        if (req.params.reponse === "12/04/2022" ) {
+            res.send(
+                ""
+            );
+        } else {
+            res.send(
+                "Mauvaise réponse…"
+            );
+        }
+
+    } catch (error) {
+        console.error('Une erreur s\'est produite :', error);
+    }
+});
+app.get('/18/:reponse', async (req, res) => {
+    try {
+        if (req.params.reponse === "11/04/2022" ) {
+            res.send(
+                ""
+            );
+        } else {
+            res.send(
+                "Mauvaise réponse…"
+            );
+        }
+
+    } catch (error) {
+        console.error('Une erreur s\'est produite :', error);
+    }
+});
+app.get('/19/:reponse', async (req, res) => {
+    try {
+        if (req.params.reponse === "anne.nanaz@sbank.fr" ) {
+            res.send(
+                ""
+            );
+        } else {
+            res.send(
+                "Mauvaise réponse…"
+            );
+        }
+
+    } catch (error) {
+        console.error('Une erreur s\'est produite :', error);
+    }
+});
+app.get('/20/:reponse', async (req, res) => {
+    try {
+        if (req.params.reponse === "Tarte aux pommes" ) {
+            res.send(
+                ""
+            );
+        } else {
+            res.send(
+                "Mauvaise réponse…"
+            );
+        }
+
+    } catch (error) {
+        console.error('Une erreur s\'est produite :', error);
+    }
+});
+app.get('/21/:reponse', async (req, res) => {
+    try {
+        if (req.params.reponse === "Nath Poléon"||req.params.reponse==="nath poléon" ) {
+            res.send(
+                ""
+            );
+        } else {
+            res.send(
+                "Mauvaise réponse…"
+            );
+        }
+
+    } catch (error) {
+        console.error('Une erreur s\'est produite :', error);
+    }
+});
+app.get('/22/:reponse', async (req, res) => {
+    try {
+        if (req.params.reponse === "anne2502" ) {
+            res.send(
+                ""
+            );
+        } else {
+            res.send(
+                "Mauvaise réponse…"
+            );
+        }
+
+    } catch (error) {
+        console.error('Une erreur s\'est produite :', error);
+    }
+});
+app.get('/23/:reponse', async (req, res) => {
+    try {
+        if (req.params.reponse === "Visite de famille à Londre" ) {
+            res.send(
+                ""
+            );
+        } else {
+            res.send(
+                "Mauvaise réponse…"
+            );
+        }
+
+    } catch (error) {
+        console.error('Une erreur s\'est produite :', error);
+    }
+});
+app.get('/24/:reponse', async (req, res) => {
+    try {
+        if (req.params.reponse === "152" ) {
+            res.send(
+                ""
+            );
+        } else {
+            res.send(
+                "Mauvaise réponse…"
+            );
+        }
+
+    } catch (error) {
+        console.error('Une erreur s\'est produite :', error);
+    }
+});
+app.get('/25/:reponse', async (req, res) => {
+    try {
+        if (req.params.reponse === "basile.m3yer@sbank.fr" ) {
+            res.send(
+                ""
+            );
+        } else {
+            res.send(
+                "Mauvaise réponse…"
+            );
+        }
+
+    } catch (error) {
+        console.error('Une erreur s\'est produite :', error);
+    }
+});
+app.get('/26/:reponse', async (req, res) => {
+    try {
+        if (req.params.reponse === "483 euros" ) {
+            res.send(
+                ""
+            );
+        } else {
+            res.send(
+                "Mauvaise réponse…"
+            );
+        }
+
+    } catch (error) {
+        console.error('Une erreur s\'est produite :', error);
+    }
+});
+app.get('/27/:reponse', async (req, res) => {
+    try {
+        if (req.params.reponse === "NIKOLAS SACOSIE"||req.params.reponse==="Nikolas Sacosie" ) {
+            res.send(
+                ""
+            );
+        } else {
+            res.send(
+                "Mauvaise réponse…"
+            );
+        }
+
+    } catch (error) {
+        console.error('Une erreur s\'est produite :', error);
+    }
+});
+app.get('/28/:reponse', async (req, res) => {
+    try {
+        if (req.params.reponse === "0645875325"|| req.params.reponse === "645875325" ) {
+            res.send(
+                ""
+            );
+        } else {
+            res.send(
+                "Mauvaise réponse…"
+            );
+        }
+
+    } catch (error) {
+        console.error('Une erreur s\'est produite :', error);
+    }
+});
+app.get('/29/:reponse', async (req, res) => {
+    try {
+        if (req.params.reponse === "007" ) {
+            res.send(
+                ""
             );
         } else {
             res.send(
